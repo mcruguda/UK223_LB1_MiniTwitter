362b19f6897fa404313f2f9e9136638a78289d18
Cyrill Augustiny <c.augustiny@gmail.com>
2024-11-27 07:51:00 +0100

    Template copied
    Copied the full template directly (replacing the old structure)

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,15 +1,15 @@
-import { Request, Response, Express } from "express";
+import { Request, Response, Express } from 'express'
 
 export class API {
   // Properties
-  app: Express;
+  app: Express
   // Constructor
   constructor(app: Express) {
-    this.app = app;
-    this.app.get("/hello", this.sayHello);
+    this.app = app
+    this.app.get('/hello', this.sayHello)
   }
   // Methods
   private sayHello(req: Request, res: Response) {
-    res.send("Hello There!");
+    res.send('Hello There!')
   }
 }
