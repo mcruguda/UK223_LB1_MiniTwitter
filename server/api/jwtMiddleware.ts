208c1bbfc879b9157d6a14fadb275b93a9659a38
Cyrill Augustiny <c.augustiny@gmail.com>
2024-12-01 14:43:50 +0100

    Added JWT middleware

---
+++ server/api/jwtMiddleware.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const secretKey = process.env.SECRETKEY
+
+export class JwtMiddleware {
+  constructor() {}
+
+  verifyToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader.split(' ')[1]
+
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' })
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: 'Failed to authenticate token' })
+      }
+
+      req.username = decoded.data
+      next()
+    })
+  }
+}
